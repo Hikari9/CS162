ac5be0708217c340805b113810ded8d8885cee9c
Rico Tiongson <thericotiongson@gmail.com>
2016-04-14 16:49:52 +0800

    Added chatroom lab.

---
+++ chatroom-lab/chat.cpp
@@ -0,0 +1,55 @@
+// compile: g++ chat.cpp -o chat 
+#include <iostream>
+#include <cerrno>
+#include <cstdio>
+#include <cstdlib>
+#include <unistd.h>
+#include "../net_client.hpp"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	if (argc < 4) {
+		printf("Some missing arguments\n");
+		printf("Format: %s <server_ip> <port> <name>\n", argv[0]);
+		return 0;
+	}
+	char* host = argv[1];
+	char* port = argv[2];
+	char* name = argv[3];
+	// try connecting to the server indefinitely with a 5 second timeout
+	net::client client;
+	while (!client) {
+		printf("Connecting to server at %s:%s...\n", host, port);
+		try {client = net::client(host, atoi(port));}
+		catch (net::socket_exception) {sleep(3);}
+	}
+	// send name to server
+	printf("Sending client information...\n");
+	client.send(name);
+	// wait for an OK from the server
+	bool ok = client.read<bool>();
+	if (!ok) {
+		// print the error message from the server
+		cerr << client.read<string>() << endl;
+		return EXIT_FAILURE;
+	}
+	// get your socket number from the server
+	int sockfd;
+	client.read(sockfd);
+	#define label "(" << sockfd << ")[" << name << "]: "
+	// MAIN CHAT: create a listening process
+	if (fork()) {
+		// child process, receive messages from server
+		string message;
+		while (client.read(message))
+			cout << '\r' << message << '\n' << label << flush;
+	} else {
+		// parent process, send messages through console input
+		string message;
+		while (getline(cin, message)) {
+			client.send(message);
+			cout << label << flush;
+		}
+	}
+}
---
+++ chatroom-lab/server.cpp
@@ -0,0 +1,120 @@
+// compile: g++ server.cpp -pthread -o server
+#include <iostream>
+#include <sstream>
+#include <cerrno>
+#include <cstdio>
+#include <cstdlib>
+#include <unistd.h>
+#include <pthread.h>
+#include <map>
+#include <vector>
+#include "../net_client.hpp"
+#include "../net_server.hpp"
+
+using namespace std;
+
+// maintain a list of clients
+vector<net::client> clients;
+
+// kick clients above this threshold
+int max_connections;
+
+// send a message to all clients
+void send_all(string message, net::client sender = -1) {
+	for (int i = 0; i < clients.size(); ++i)
+		if (clients[i] != sender)
+			clients[i].send(message);
+	clog << message << endl; // have a log in the console
+}
+
+// thread that listens to clients
+void* client_listener(void* args);
+
+int main(int argc, char* argv[]) {
+	// check validity of arguments
+	if (argc == 1) {
+		printf("Some missing arguments\n");
+		printf("Format: %s <max_connections> <port>\n", argv[0]);
+		return 0;
+	}
+	::max_connections = atoi(argv[1]);
+	int port = atoi(argv[2]);
+	// create server using port, listen up to max_connections
+	net::server server(port, max_connections);
+	int sockfd = (int) server;	// we can get sockfd of server
+	printf("Server: created server at %s (port %s) [sockfd=%d]\n", server.ip(), argv[1], sockfd);
+	// maintain a a pthread vector that will be destructed on exit
+	vector<pthread_t> pthreads;
+	// indefinitely accept clients
+	while (true) {
+		printf("Server: accepting clients...\n");
+		// accept a client from the server socket
+		net::client client = server.accept();
+		printf("connected to client socket [%d]\n", (int) client);
+		// create a dedicated thread for client messages
+		pthreads.push_back(pthread_t());
+		int error;
+		if (error = pthread_create(&pthreads.back(), NULL, &client_listener, reinterpret_cast<void*>((int) client))) {
+			errno = error;
+			perror("pthread_create()");
+		}
+		sleep(1);
+	}
+}
+
+// thread that listens to client messages
+void* client_listener(void* args) {
+	net::client client = *(int*) &args;
+	const char* ip = client.ip();
+	printf("acquiring name of client [%s]...\n", ip);
+	// get name of the client
+	string name;
+	client.read(name);
+	if (client.good()) {
+		if (clients.size() >= max_connections) {
+			// server already full
+			client.send(false);
+			client.send("server is already full");
+		} else {
+			// client can join
+			clients.push_back(client);
+			string label;
+			try {
+				client.send(true);
+				client.send((int) client); // send the socket number of this client as well
+				{
+					// prepare the label of this client: "(sockfd)[name]: "
+					ostringstream oss;
+					oss << "(" << (int) client << ")[" << name << "]";
+					label = oss.str();
+				}
+				{
+					// send a message to everyone that client has joined
+					ostringstream oss;
+					oss << name << " entered the room (sockfd=" << (int) client << ")";
+					send_all(oss.str());
+				}
+				// indefinitely listen to messages
+				string message;
+				while (client.read(message)) {
+					if (!message.empty()) {
+						ostringstream oss;
+						send_all(label + ": " + message, client);
+					}
+				}
+			} catch (net::socket_exception ex) {
+				cerr << ex.what() << endl;
+			}
+			// client has disconnected
+			for (int i = 0; i < clients.size(); ++i) {
+				if (!clients[i].good()) {
+					clients[i--] = clients.back();
+					clients.pop_back();
+				}
+			}
+			ostringstream oss;
+			oss << name << " has left the room (sockfd=" << (int) client << ")";
+			send_all(oss.str());
+		}		
+	}
+}
