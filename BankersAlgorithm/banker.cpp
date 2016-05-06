8719e0c7c16faccf150809c7259ae7a65d22ab53
Rico Tiongson <thericotiongson@gmail.com>
2016-05-06 10:44:43 +0800

    Increased code readability.

--- BankersAlgorithm/banker.cpp
+++ BankersAlgorithm/banker.cpp
@@ -23,7 +23,7 @@ struct process {
 
 // check if resources are available for use
 
-bool available(const resource& need, const resource& pool) {
+bool available(resource need, resource pool) {
 	for (int i = 0; i < need.size(); ++i)
 		if (need[i] > pool[i])
 			return false;
@@ -34,57 +34,73 @@ bool available(const resource& need, const resource& pool) {
  * @brief       performs banker's algorithm for process ordering to avoid deadlocks
  * @details     enqueued implementation of banker's algorithm O(n^2)
  * @param[in]	pro		a queue of processes that want to allocate resources
- * @param[in]	res		a list of available resources
+ * @param[in]	pool	a list of available resources
  * @returns     a list of process IDs indicating the preferred order of process execution
  *              will have a size less than the number of processes if a deadlock occurs
  */
 
-vector<int> banker(process_queue pro, resource res) {
+vector<int> banker(process_queue pro, resource pool) {
+
 	vector<int> order;
-	for (int checked = 0; checked < pro.size(); pro.pop_front()) {
-		// get the process in front of the queue
-		const process& cur = pro.front();
-		if (available(cur.need, res)) {
-			// resources are available for current process
-			order.push_back(cur.id);
-			for (int i = 0; i < res.size(); ++i)
-				res[i] += cur.has[i];
-			checked = 0; // reset process count
-		}
-		else {
-			// cannot allocate, repush process
-			pro.push_back(cur);
+	int checked = 0; // counter to check if the queued processes have repeated
+
+	while (checked < pro.size()) {
+
+		// try processing the frontmost process
+		process current = pro.front();
+		pro.pop_front();
+
+		if (available(current.need, pool)) {
+
+			// allocate needs, then immediately return
+			order.push_back(current.id);
+			for (int i = 0; i < pool.size(); ++i)
+				pool[i] += current.has[i];
+
+			// reset process count
+			checked = 0;
+
+		} else {
+
+			// cannot allocate, repush process to the back
+			pro.push_back(current);
 			checked++;
+
 		}
+
 	}
+
 	return order;
 }
 
 int main() {
 	int t, n, m;
-	scanf("%d", &t);
+	cin >> t;
 	while (t--) {
-		scanf("%d%d", &n, &m);
-		resource res(n);
+		cin >> n >> m;
+		resource pool(n);
 		process_queue pro(n, process(m));
-		for (int i = 0; i < m; ++i)
-			scanf("%d", &res[i]);
-		for (int i = 0; i < n; ++i)
-			pro[i].id = i + 1;
-		for (int i = 0; i < n; ++i)
-			for (int j = 0; j < m; ++j)
-				scanf("%d", &pro[i].has[j]);
+
+		// input
+		for (int i = 0; i < m; ++i) cin >> pool[i];
+		for (int i = 0; i < n; ++i) pro[i].id = i + 1;
 		for (int i = 0; i < n; ++i)
 			for (int j = 0; j < m; ++j)
-				scanf("%d", &pro[i].need[j]);
-		// print the ordering of process IDs
-		vector<int> order = banker(pro, res);
-		printf(order.size() < n ? "UNSAFE" : "SAFE");
-		for (int i = 0; i < order.size(); ++i) {
-			if (i) printf("-");
-			else printf(" ");
-			printf("%d", order[i]);
-		}
-		printf("\n");
+				cin >> pro[i].has[j];
+		for (int i = 0; i < n; ++i)
+			for (int j = 0; j < m; ++j)
+				cin >> pro[i].need[j];
+
+		// get ordering from banker's algo
+		vector<int> order = banker(pro, pool);
+
+		// SAFE or UNSAFE?
+		if (order.size() == n) cout << "SAFE";
+		else  /* incomplete */ cout << "UNSAFE";
+		
+		// output ordering from banker's algo
+		for (int i = 0; i < order.size(); ++i)
+			cout << (i ? "-" : " ") << order[i];
+		cout << endl;
 	}
 }
